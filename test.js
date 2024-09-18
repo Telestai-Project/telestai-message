c8e81303602dc426a417f1e6ab399e4208e0ba0f
dev0614 <shimamuratakehiko44@gmail.com>
2024-09-18 19:16:17 -0400

    test

--- test.js
+++ test.js
@@ -1,21 +1,36 @@
-const { verifyMessage } = require("./dist/main");
+const { verifyMessage, sign } = require("./dist/main");
+const bitcoin = require('bitcoinjs-lib');
+const ECPairFactory = require('ecpair').ECPairFactory;
+const ecc = require('tiny-secp256k1');
 
-const address = "TcsCu4yjc2GFZCXPVkxQ6E54MWCHkdT9z2";
-const message = "902392";
+const ECPair = ECPairFactory(ecc);
+
+const wifPrivateKey = "your_wif_private_key";
+const keyPair = ECPair.fromWIF(wifPrivateKey, bitcoin.networks.bitcoin);
+const decodedPrivateKey = keyPair.privateKey;
+
+const privateKeyBuffer = Buffer.from(decodedPrivateKey);
+
+const address = "you_public_key";
+const message = "something";
 const signature =
-  "IM2pxWRW2csCRedLajB7ji7swNniPu2DIKPg6FX86Su4dnr5FNkZNBMSztFICDRnpl7lgBmTRN2aWLLToR17TYk=";
+  "signature";
 
 test("Verify valid message signature", () => {
   const result = verifyMessage(message, address, signature);
-
   expect(result).toBe(true);
 });
 
-test("Verify unvalid message signature", () => {
+test("Verify invalid message signature", () => {
   const result = verifyMessage(
     message + " change the message",
     address,
     signature
   );
   expect(result).toBe(false);
+});
+
+test("Sign message", () => {
+  const result = sign("Hello", privateKeyBuffer);
+  console.log(result);
 });
